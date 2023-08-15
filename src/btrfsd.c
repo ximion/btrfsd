b7ca1e5a134f6a3567d9747909c4533a8faf7362
Matthias Klumpp <matthias@tenstral.net>
2023-08-15 21:26:28 +0200

    Add autoformat configuration

--- src/btrfsd.c
+++ src/btrfsd.c
@@ -13,43 +13,43 @@
 int
 main (int argc, char **argv)
 {
-	g_autoptr(GOptionContext) option_context = NULL;
+    g_autoptr(GOptionContext) option_context = NULL;
     g_autoptr(GError) error = NULL;
-	gboolean ret;
-	gboolean verbose = FALSE;
-	gboolean show_version = FALSE;
+    gboolean ret;
+    gboolean verbose = FALSE;
+    gboolean show_version = FALSE;
 
-	const GOptionEntry options[] = {
-		{ "verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose,
-			"Show extra debugging information", NULL },
-		{ "version", '\0', 0, G_OPTION_ARG_NONE, &show_version,
-			"Show the program version.", NULL },
-		{ NULL}
-	};
+    const GOptionEntry options[] = {
+        {"verbose",
+         'v', 0,
+         G_OPTION_ARG_NONE, &verbose,
+         "Show extra debugging information", NULL },
+        { "version",        '\0', 0, G_OPTION_ARG_NONE, &show_version, "Show the program version.", NULL },
+        { NULL}
+    };
 
-	setlocale (LC_ALL, "");
-	bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
-	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-	textdomain (GETTEXT_PACKAGE);
-	option_context = g_option_context_new ("Btrfs maintenance helper.");
+    setlocale (LC_ALL, "");
+    bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
+    bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+    textdomain (GETTEXT_PACKAGE);
+    option_context = g_option_context_new ("Btrfs maintenance helper.");
 
-	g_option_context_add_main_entries (option_context, options, NULL);
-	ret = g_option_context_parse (option_context, &argc, &argv, &error);
-	if (!ret) {
-		g_print ("%s: %s\n",
-                 "Failed to parse arguments", error->message);
-		return EXIT_FAILURE;
-	}
+    g_option_context_add_main_entries (option_context, options, NULL);
+    ret = g_option_context_parse (option_context, &argc, &argv, &error);
+    if (!ret) {
+        g_print ("%s: %s\n", "Failed to parse arguments", error->message);
+        return EXIT_FAILURE;
+    }
 
-	if (verbose)
-		g_setenv ("G_MESSAGES_DEBUG", "all", TRUE);
+    if (verbose)
+        g_setenv ("G_MESSAGES_DEBUG", "all", TRUE);
 
-	if (show_version) {
-		g_print ("Btrfsd version: %s", PACKAGE_VERSION);
-		return EXIT_SUCCESS;
-	}
+    if (show_version) {
+        g_print ("Btrfsd version: %s", PACKAGE_VERSION);
+        return EXIT_SUCCESS;
+    }
 
-	/* TODO */
+    /* TODO */
 
     return EXIT_SUCCESS;
 }
